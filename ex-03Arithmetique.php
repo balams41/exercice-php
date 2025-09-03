0894014bb36db4201de164dea00af00993a7a5c5
Kabendou41 <balamini@DESKTOP-TDGK0ID.>
2025-09-03 13:41:58 +0200

    suitecours

---
+++ ex-03Arithmetique.php
@@ -0,0 +1,47 @@
+<h1>Exercice1</h1>
+<?php
+ $a = 10;
+ $b = 5;
+  echo "<p>Ma variable a = $a et ma variable b = $b</p>";
+
+        $total = $a + $b;
+        echo "<p>Ma variable a + ma variable b = $total</p>";
+        
+        $total = $a - $b;
+        echo "<p>Ma variable a - ma variable b = $total</p>";
+
+        $total = $a * $b;
+        echo "<p>Ma variable a x ma variable b = $total</p>";
+
+        $total = $a / $b;
+        echo "<p>Ma variable a ÷ ma variable b = $total</p>";
+
+        $total = $a % $b;
+?>
+
+<h2>Exercice2</h2>
+
+<?php
+   $a = 10;
+   $b = 5;
+       $a+= $b;
+     
+
+        $a -= $b;
+
+        $a *= $b;
+       
+
+        $a /= $b;
+     
+
+        $a %= $b;
+      
+?>
+
+<h2>exercice3</h2>
+
+<?php
+  $i = 0;
+  
+?>
---
+++ ex-condition.php
@@ -0,0 +1,12 @@
+
+<H2>Exerice2</H2>
+<?php
+    $age1 = 22;
+    $age2 = 100;
+    
+    if($age1, $age2){
+        echo '<p>'
+        else
+    }
+
+?>
